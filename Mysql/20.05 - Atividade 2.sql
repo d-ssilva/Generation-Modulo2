19758e3f35b582dc05da1eb9f4dae6a3d685964b
Danilo <danilo.dos.s.silva@gmail.com>
2021-05-20 13:59:27 -0300

    Primeiras atividades

---
+++ Mysql/20.05 - Atividade 2.sql
@@ -0,0 +1,21 @@
+create database db_ecomerce;
+use db_ecomerce;
+create table produtos(
+id bigint,
+primary key(id),
+nome varchar(255) not null,
+quantidade bigint,
+preco decimal not null,
+descricao varchar(999)
+);
+
+insert into produtos (nome, quantidade, preco, descricao) values
+("Poster", 5, 25.00, "Xenomorph"),
+("Desodorante", 6, 12.50, "Axe"),
+("Talheres", 20,60.00, ""),
+("Capinha celular", 20, 15.00, "Diversos modelos"),
+("Canetas", 50, 20.00, "Caixas fechadas / Diversas cores"),
+("Caderno", 12, 15.60, ""),
+("Toalhas de banho", 50, 20.00, ""),
+("Lixeiras", 20, 12.60, ""),
+("Balm", 10, 20, "");
