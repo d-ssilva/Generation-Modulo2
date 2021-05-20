f48e4f403b292ff3d937b716408304a7168af56c
Danilo <danilo.dos.s.silva@gmail.com>
2021-05-20 14:32:36 -0300

    Correção do 'auto_increment'

--- Mysql/20.05 - Atividade 2.sql
+++ Mysql/20.05 - Atividade 2.sql
@@ -1,13 +1,16 @@
 create database db_ecomerce;
 use db_ecomerce;
+
 create table produtos(
-id bigint,
+id bigint auto_increment,
 primary key(id),
 nome varchar(255) not null,
 quantidade bigint,
 preco decimal not null,
 descricao varchar(999)
 );
+
+drop table produtos;
 
 insert into produtos (nome, quantidade, preco, descricao) values
 ("Poster", 5, 25.00, "Xenomorph"),
@@ -19,3 +22,6 @@ insert into produtos (nome, quantidade, preco, descricao) values
 ("Toalhas de banho", 50, 20.00, ""),
 ("Lixeiras", 20, 12.60, ""),
 ("Balm", 10, 20, "");
+
+select * from produtos where preco > 500;
+select * from produtos where preco < 500;
