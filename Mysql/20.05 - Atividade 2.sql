4c621c046e7bb6d11b9e52158d91ecca4cb3ff8f
Danilo <danilo.dos.s.silva@gmail.com>
2021-05-21 09:37:52 -0300

    Correção nos atributos de preço

--- Mysql/20.05 - Atividade 2.sql
+++ Mysql/20.05 - Atividade 2.sql
@@ -6,7 +6,7 @@ id bigint auto_increment,
 primary key(id),
 nome varchar(255) not null,
 quantidade bigint,
-preco decimal not null,
+preco decimal(10,2) not null,
 descricao varchar(999)
 );
 
@@ -15,13 +15,14 @@ drop table produtos;
 insert into produtos (nome, quantidade, preco, descricao) values
 ("Poster", 5, 25.00, "Xenomorph"),
 ("Desodorante", 6, 12.50, "Axe"),
-("Talheres", 20,60.00, ""),
+("Talheres", 20, 60.00, "Tramontina"),
 ("Capinha celular", 20, 15.00, "Diversos modelos"),
 ("Canetas", 50, 20.00, "Caixas fechadas / Diversas cores"),
-("Caderno", 12, 15.60, ""),
-("Toalhas de banho", 50, 20.00, ""),
-("Lixeiras", 20, 12.60, ""),
-("Balm", 10, 20, "");
+("Caderno", 12, 15.60, "n/a"),
+("Toalhas de banho", 50, 20.00, "n/a"),
+("Lixeiras", 20, 12.60, "n/a"),
+("Balm", 10, 20.00, "n/a");
 
+select * from produtos;
 select * from produtos where preco > 500;
 select * from produtos where preco < 500;
